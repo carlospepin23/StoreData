75f7b7e1a6f6556a8acd995f9001f261ece7e484
carlospepin23 <carlos.pepin1@upr.edu>
2024-08-06 21:47:17 -0400

    front end draft

--- FRONTEND/eStore-finalProject/src/components/EntityCard.jsx
+++ FRONTEND/eStore-finalProject/src/components/EntityCard.jsx
@@ -8,7 +8,8 @@ function EntityCard({ entity }) {
       
       {entity.price && <p>Price: ${entity.price}</p>}
       {entity.stock && <p>Stock: {entity.stock}</p>}
-      {entity.email && <p>Email: {entity.email}</p>}
+      {entity.email && <p>Seller <br /> Email: {entity.email}</p>}
+      {/* later i can add a badge saying that its a seller  */}
       {entity.sales && <p>Sales: {entity.sales}</p>}
     </div>
   );
